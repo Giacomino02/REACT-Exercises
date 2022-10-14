11ebe00b0ee28dc85ea38cf4bdf7b65e2c495955
Giacomo Granducci <ggranducci@gmail.com>
2022-10-14 09:44:26 +0200

    add: props-02

--- my-app/src/welcome.js
+++ my-app/src/welcome.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 export class Welcome extends React.Component {
-    render(){
-        return <p>Welcome, {this.props.name}!</p>
+    render() {
+        return (
+            <>
+                {
+                    this.props.name 
+                        ? <p>Welcome, {this.props.name}!</p>
+                        : <p>Welcome, user!</p>
+                }
+            </>
+        )
     }
 }
