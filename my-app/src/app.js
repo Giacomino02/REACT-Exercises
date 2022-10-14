a4506729338c0c732d2560e0f967689b87149319
Giacomo Granducci <ggranducci@gmail.com>
2022-10-14 10:12:59 +0200

    add: props-04

--- my-app/src/app.js
+++ my-app/src/app.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Hello } from './hello';
 import { Welcome } from './welcome';
 
+const name = <strong>Alfio</strong>
 export class App extends React.Component {
     render() {
         return (
             <div>
                 <Hello />
-                <Welcome name="Alfio" age= "20"/>
+                <Welcome name={name} age= "20"/>
             </div>
         )
-
     }
 }
