a125ae6b241395ebe267dc8ed9640b877f2e41e6
Giacomo Granducci <ggranducci@gmail.com>
2022-10-28 09:21:23 +0200

    add: f.c-01

--- my-app/src/welcome.js
+++ my-app/src/welcome.js
@@ -1,17 +1,14 @@
 import React from "react";
 import { Age } from "./age";
 
-export class Welcome extends React.Component {
-    render() {
-        return (
-            <>
-                {
-                    this.props.name 
-                        ? <p className="welcome">Welcome, {this.props.name}!</p>
-                        : <p>Welcome, user!</p>
-                }
-                {this.props.age > 18 && <Age age={this.props.age}/> || <p>You're very young!</p>}
-            </>
-        )
-    }
+export function Welcome(props) {
+    return (
+        <>
+
+            <p className="welcome">Welcome, {props.name}!</p>
+            <p>Welcome, user!</p>
+
+            {props.age > 18 && <Age age={props.age} /> || <p>You're very young!</p>}
+        </>
+    )
 }
